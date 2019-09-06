698a6667480d470c0aece5be7318d7d4e1fb68c8
Kurt Ma <gladiator8072@gmail.com>
2019-09-06 13:27:59 +1200

    varied-dim

---
+++ source/hsicbt/task/task_varieddim.py
@@ -0,0 +1,121 @@
+from . import *
+
+def task_varieddim_func(config_dict):
+
+    model_filename = config_dict['model_file']
+
+    if config_dict['do_training_hsic']:
+
+        config_dict['hidden_width'] = 8
+        config_dict['exp_index'] = 1
+        config_dict['model_file'] = "{}-{:04d}.pt".format(
+            os.path.splitext(model_filename)[0], config_dict['exp_index'])
+        training_hsic(config_dict) 
+
+        config_dict['hidden_width'] = 32
+        config_dict['exp_index'] = 2
+        config_dict['model_file'] = "{}-{:04d}.pt".format(
+            os.path.splitext(model_filename)[0], config_dict['exp_index'])
+        training_hsic(config_dict) 
+
+        config_dict['hidden_width'] = 64
+        config_dict['exp_index'] = 3
+        config_dict['model_file'] = "{}-{:04d}.pt".format(
+            os.path.splitext(model_filename)[0], config_dict['exp_index'])
+        training_hsic(config_dict) 
+
+    if config_dict['do_training_format']:
+
+        config_dict['batch_size'] = 128
+        config_dict['exp_index'] = 1
+        config_dict['hidden_width'] = 8
+        config_dict['model_file'] = "{}-{:04d}.pt".format(
+            os.path.splitext(model_filename)[0], config_dict['exp_index'])
+        training_format(config_dict)
+
+        config_dict['exp_index'] = 2
+        config_dict['hidden_width'] = 32
+        config_dict['model_file'] = "{}-{:04d}.pt".format(
+            os.path.splitext(model_filename)[0], config_dict['exp_index'])
+        training_format(config_dict)
+
+        config_dict['exp_index'] = 3
+        config_dict['hidden_width'] = 64
+        config_dict['model_file'] = "{}-{:04d}.pt".format(
+            os.path.splitext(model_filename)[0], config_dict['exp_index'])
+        training_format(config_dict)
+
+        
+    if config_dict['do_plot']:
+
+        try:
+            out_standard_batch_001 = load_logs(get_batch_log_filepath(
+                config_dict['task'], TTYPE_FORMAT, config_dict['data_code'], 1))
+            out_standard_batch_005 = load_logs(get_batch_log_filepath(
+                config_dict['task'], TTYPE_FORMAT, config_dict['data_code'], 2))
+            out_standard_batch_010 = load_logs(get_batch_log_filepath(
+                config_dict['task'], TTYPE_FORMAT, config_dict['data_code'], 3))
+            out_standard_epoch_001 = load_logs(get_epoch_log_filepath(
+                config_dict['task'], TTYPE_FORMAT, config_dict['data_code'], 1))
+            out_standard_epoch_005 = load_logs(get_epoch_log_filepath(
+                config_dict['task'], TTYPE_FORMAT, config_dict['data_code'], 2))
+            out_standard_epoch_010 = load_logs(get_epoch_log_filepath(
+                config_dict['task'], TTYPE_FORMAT, config_dict['data_code'], 3))
+
+        except IOError as e:
+            print_highlight("{}.\nPlease do training by setting do_training key to True in config. Program quits.".format(e), 'red')
+            quit()
+
+        input_batch_list = [out_standard_batch_001, out_standard_batch_005, out_standard_batch_010]
+        input_epoch_list = [out_standard_epoch_001, out_standard_epoch_005, out_standard_epoch_010]
+        label_list = ['dim-8', 'dim-32', 'dim-64']
+
+        metadata = {
+            'title':'HSIC(X, Z_L) of Varied-dim',
+            'xlabel': 'epochs',
+            'ylabel': 'HSIC(X, Z_L)',
+            'label': label_list
+        }
+        plot.plot_batches_log(input_batch_list, 'batch_hsic_hx', metadata)
+        plot.save_figure(get_exp_path("varied-dim-hsic_xz-{}.{}".format(
+            config_dict['data_code'], config_dict['ext'])))
+
+        metadata = {
+            'title':'HSIC(Y, Z_L) of Varied-dim',
+            'xlabel': 'epochs',
+            'ylabel': 'HSIC(Y, Z_L)',
+            'label': label_list
+        }
+        plot.plot_batches_log(input_batch_list, 'batch_hsic_hy', metadata)
+        plot.save_figure(get_exp_path("varied-dim-hsic_yz-{}.{}".format(
+            config_dict['data_code'], config_dict['ext'])))
+
+        metadata = {
+            'title':'format-train of Varied-dim',
+            'xlabel': 'epochs',
+            'ylabel': 'training accuracy',
+            'label': label_list
+        }
+        plot.plot_batches_log(input_batch_list, 'batch_acc', metadata)
+        plot.save_figure(get_exp_path("varied-dim-acc-{}.{}".format(
+            config_dict['data_code'], config_dict['ext'])))
+
+        metadata = {
+            'title':'format-train of Varied-dim',
+            'xlabel': 'epochs',
+            'ylabel': 'training loss',
+            'label': label_list
+        }
+        plot.plot_batches_log(input_batch_list, 'batch_loss', metadata)
+        plot.save_figure(get_exp_path("varied-dim-loss-{}.{}".format(
+            config_dict['data_code'], config_dict['ext'])))
+
+        metadata = {
+            'title':'{} test performance of Varied-dim'.format(config_dict['data_code']),
+            'xlabel': 'epochs',
+            'ylabel': 'test accurarcy',
+            'label': label_list
+        }
+        plot.plot_epoch_log(input_epoch_list, 'test_acc', metadata)
+        plot.save_figure(get_exp_path("{}-epoch-test-acc.{}".format(
+            get_plot_filename(config_dict), config_dict['ext'])))
